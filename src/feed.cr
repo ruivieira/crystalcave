ed4825a071d6f648d5e5bb4f74a4c7ec80b14e13
Rui Vieira <ruidevieira@googlemail.com>
2016-11-24 13:45:29 +0000

    Added items parsing to RSS feeds

--- src/feed.cr
+++ src/feed.cr
@@ -1,11 +1,20 @@
 require "http/client"
 require "xml"
 
+class Item
+  property title, link, pubDate, comments, description
+
+  def initialize
+    @title = ""
+  end
+end
+
 class RSS
-  property version
+  property version, items
 
   def initialize
     @version = "2.0"
+    @items = Array(Item).new
   end
 
   def self.parse(url : String)
@@ -20,6 +29,16 @@ class RSS
       result.version = version.as(XML::Node)["version"].as(String)
     end
 
+    items = feed.xpath("//rss/channel/item").as(XML::NodeSet)
+    result.items = items.map { |c|
+      item = Item.new
+      title = c.xpath_node("title").as(XML::Node)
+      if title
+        item.title = title.text.as(String)
+      end
+      item
+    }
+
     return result
   end
 
@@ -32,3 +51,5 @@ end
 feed = RSS.parse "https://news.ycombinator.com/rss"
 
 puts feed.to_s
+
+feed.items.each { |e| puts e.title }
